9ced236cf3724fc83b4d0af307d1912c0f25ae7b
= <=>
2025-10-17 09:33:08 +0800

    initial commit

---
+++ assets/js/functions.js
@@ -0,0 +1,5 @@
+let $darkmodebtn = document.getElementById("dark-modebtn");
+let $NameOfUser = document.getElementById("name-of-user");
+let $NavBar = document.querySelector(".navbar");
+
+$darkmodebtn.addEventListener("click", function () {});
